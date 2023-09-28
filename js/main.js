2d16fbddb7b03ca31bd9f6736f7ea190699cd3f9
Anna Aksenov <annaaksenov8@gmail.com>
2023-09-28 18:51:44 +0000

    Able to recieve 10 cats and store them in data.queue

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function handleStart(e) {
   // Need to load 10 cats
 
   swap('guess-view');
-  getCats();
+  getTenCats();
 }
 
 const gView = document.querySelector('div[data-view="guess-view"]');
@@ -25,7 +25,7 @@ function swap(view) {
 // const img = document.querySelector('img');
 // query the multiple choice
 
-function getCats(cat) {
+function getTenCats() {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://api.thecatapi.com/v1/images/search?has_breeds=1&limit=10&api_key=live_NO1khTMuP3C8aJanIQAydCR1eolXjepWGEGEpBl2zkQDPctiac7A92lHoSqOpCdz');
   xhr.responseType = 'json';
@@ -33,9 +33,9 @@ function getCats(cat) {
     // console.log(xhr.status);
     // console.log(xhr.response);
     const json = xhr.response;
-    for (let i = 0; i < json.length; i++) {
-      data.queue = json[i];
-    }
+    data.queue = json;
+    // console.log('data.queue', data.queue[0].name);
+    // for (let i = 0; i < json.length; i++) {}
   });
   xhr.send();
 }
